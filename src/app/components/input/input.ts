ef8301e63f1700e6a88143ae517e3fefa9c0b22b
matheustsx <matheusbatistachavante@gmail.com>
2025-08-21 17:33:41 -0300

    blablabla

--- src/app/components/input/input.ts
+++ src/app/components/input/input.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-input',
-  imports: [MatDatepickerModule, MatLabel, MatFormFieldModule],
+  imports: [MatDatepickerModule, MatLabel, MatFormFieldModule, MatInputModule, MatNativeDateModule],
   providers: [provideNativeDateAdapter()],
   templateUrl: './input.html',
   styleUrl: './input.scss',
